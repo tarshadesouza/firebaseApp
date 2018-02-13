77b31c4ba06d21477770350568e06a0bb2a67ddb
Tarsha Maykere de Souza <Tarsha@Tarshas-MacBook-Pro.local>
2018-02-13 18:04:39 +0100

    Initial Commit

---
+++ firebaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseApp
+//
+//  Created by Tarsha Maykere de Souza on 2/13/18.
+//  Copyright © 2018 Tarsha Maykere de Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
